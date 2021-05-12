45295e1137fe8206455cb29264c9785919114867
ArkadiiIl <odinls91@gmail.com>
2021-05-12 17:05:30 +0200

    Solution

--- app/src/main/java/com/arkadii/buttontest/MainActivity.kt
+++ app/src/main/java/com/arkadii/buttontest/MainActivity.kt
@@ -4,7 +4,10 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
     private var startX = 0f
@@ -17,6 +20,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val moveButton = findViewById<FloatingActionButton>(R.id.moveButton)
         moveButton.setOnTouchListener { view, event ->
+            val uncheckParent = view.parent
+            if(uncheckParent is View) {
+                val parentLocation = IntArray(2)
+                uncheckParent.getLocationOnScreen(parentLocation)
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    val location = IntArray(2) {0}
+                    view.getLocationInWindow(location)
+                    startX = location[0].toFloat()
+                    startY = location[1].toFloat()
+                }
+                MotionEvent.ACTION_UP -> {
+                    view.animate().let { animator ->
+                        animator.x(startX)
+                        animator.y(abs(startY - parentLocation[1]))
+                        animator.duration = 0
+                        animator.start()
+                    }
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    view.animate().let { animator ->
+                        animator.x(event.rawX)
+                        animator.y(event.rawY - parentLocation[1])
+                        animator.duration = 0
+                        animator.start()
+                    }
+                }
+            }
+            }
+            false
+        }
+
+
+        val moveButton2 = findViewById<FloatingActionButton>(R.id.moveButton2)
+        moveButton2.setOnTouchListener {view, event ->
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
                     val location = IntArray(2) {0}
@@ -44,4 +82,5 @@ class MainActivity : AppCompatActivity() {
             false
         }
     }
+
 }
